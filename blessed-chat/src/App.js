71e82bdb78c400e1f6d7c01255d8ee598587e0e7
Francisco Pulido <67913723+lolways@users.noreply.github.com>
2021-06-27 17:56:36 -0300

    Update App.js

--- blessed-chat/src/App.js
+++ blessed-chat/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+ //dependencias de firebase
+ import firebase from "firebase/app";
+ import 'firebase/auth'
+ import 'firebase/firestore'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       
     </div>
   );
 }
