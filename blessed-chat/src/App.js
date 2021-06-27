9980bf441f889be04bb90f0b513e70cf94bc2f83
Francisco Pulido <67913723+lolways@users.noreply.github.com>
2021-06-27 20:17:43 -0300

    inicio de sesion

--- blessed-chat/src/App.js
+++ blessed-chat/src/App.js
@@ -3,6 +3,15 @@
  import 'firebase/auth'
  import 'firebase/firestore'
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCJcvP96Yp4mflATereZ9WmLHMd3hCLTXY",
+    authDomain: "blessed-chat-588b7.firebaseapp.com",
+    projectId: "blessed-chat-588b7",
+    storageBucket: "blessed-chat-588b7.appspot.com",
+    messagingSenderId: "21934670709",
+    appId: "1:21934670709:web:751e7041d36bc4ed76893c"
+})
+
 function App() {
   return (
     <div>
