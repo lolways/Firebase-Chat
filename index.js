df1f473213e381b381e4cc73205eb3e9624181a9
Francisco Pulido <67913723+lolways@users.noreply.github.com>
2021-07-04 22:32:57 -0300

    chat refresh, nombre,

--- index.js
+++ index.js
@@ -108,57 +108,183 @@ window.onload = function (){
         
         //creacion de chat
         create_chat(){
-            
+
             var parent = this;
-            
-            var title_container = document.getElementById('title_container')
+
+            var title_container = document.createElement('div')
             var title = document.getElementById('title')
             title_container.classList.add('chat_title_container')
-            
+
             title.classList.add('chat_title')
-      
+
             var chat_container = document.createElement('div')
-            chat_container.setAttribute('id', 'chat_container')
-      
+            chat_container.setAttribute('id', 'chat_conteiner')
+
             var chat_inner_container = document.createElement('div')
             chat_inner_container.setAttribute('id', 'chat_inner_container')
-      
+
             var chat_content_container = document.createElement('div')
             chat_content_container.setAttribute('id', 'chat_content_container')
-      
+
             var chat_input_container = document.createElement('div')
             chat_input_container.setAttribute('id', 'chat_input_container')
-      
+
             var chat_input_send = document.createElement('button')
-            chat_input_send.setAttribute('id', 'chat_input_send')
+            chat_input_send.setAttribute('id', 'chat_inpt_send')
             chat_input_send.setAttribute('disabled', true)
-            chat_input_send.innerHTML = `<i class="far fa-paper-plane"></i>`
-      
+            chat_input_send.innerHTML = <i class="far fa-paper-plane"></i>
+            
             var chat_input = document.createElement('input')
             chat_input.setAttribute('id', 'chat_input')
-            
-            //largo del mensaje
+
             chat_input.setAttribute('maxlength', 1000)
-            
-            // obtener el nombre del usuario
-            chat_input.placeholder = `${parent.get_name()}. Say something...`
-            chat_input.onkeyup  = function(){
-              if(chat_input.value.length > 0){
-                chat_input_send.removeAttribute('disabled')
-                chat_input_send.classList.add('enabled')
-                chat_input_send.onclick = function(){
-                  
-                    chat_input_send.setAttribute('disabled', true)
-                    chat_input_send.classList.remove('enabled')
-                 
-                    if(chat_input.value.length <= 0){
-                        return
-                     }
+
+            //obtener el nombre del usuario
+            chat_input.placeholder = `${parent.get_name()}. Escriba algo`
+            chat_input.onkeyup = function(){
+
+                if(chat_input.value.length > 0){
+                    chat_input_send.removeAttribute('disabled')
+                    chat_input_send.classList.add('enabled')
+                    chat_input_send.onclick = function(){
+
+                        chat_input_send.setAttribute('disabled', true)
+                        chat_input_send.setAttribute('enabled')
+
+                        if(chat_input.value.length <=0){
+                            return
+                        }
+
+                        parent.create_load('chat_content_container')
+
+                        parent.send_message(chat_input.value)
+
+                        chat_input.value = ''
+
+                        chat_input.focus()
+
                     }
+                }else{
+                    chat_input_send.classList.remove('enabeld')
                 }
             }
-            };
 
-            
+            var chat_logout_container = document.createElement('div')
+            chat_logout_container.setAttribute('id', 'chat_logout_container')
+
+            var chat_logout = document.createElement('button')
+            chat_logout.setAttribute('id', 'chat_logout')
+            chat_logout.textContent = `${parent.get_name()} salir`
+
+            chat_logout.onclick = function(){
+                localStorage.clear()
+
+                parent.home
+            }
+
+            chat_logout_container.append(chat_logout)
+            chat_input_container.append(chat_input, chat_input_send)
+            chat_inner_container.append(chat_content_container, chat_input_container, chat_logout_container)
+            chat_container.append(chat_inner_container)
+            document.body.append(chat_container)
+
+            parent.create_load('chat_content_container')
+
+            parent.refresh_chat()
+
+        };
+
+        //guardado de nombre
+        save_name(name){
+            localStorage.setItem('name', name)
+        };
+
+        //enviar mensaje
+        send_message(message){
+
+            var parent = this
+
+            if(parent.get_name()== null && message == null){
+                return
+            }
+
+            db.ref('chats/').once('value', function(message_object){
+
+                var index = parseFloat(message_object.numChildren()) + 1
+                db.ref('chats/'+ `message_${index}`).set({
+                    name: parent.get_name(),
+                    message: message,
+                    index: index
+                })
+                .then(function(){
+                    parent.refresh_chat()
+                })
+            })
+        };
+
+        //obtener nombre
+        get_name(){
+            if(localStorage.getItem('name') !=null){
+                return localStorage.getItem('name')
+
+            }else{
+                this.home()
+                return null
+            }
+        };
+
+        //refrescar chat
+        refresh_chat(){
+            var chat_content_container = document.getElementById('chat_content_container')
+
+            db.ref('chats/').on('value', function(message_object){
+
+                chat_content_container.innerHTML = ''
+
+                if(message_object.numChildren()==0){
+                    return
+                }
+
+                var messages = Object.values(message_object.val());
+                var guide = []
+                var unorderd = []
+                var ordered = []
+
+                for(var i,i = 0; i< messages.length; i++){
+
+                    guide.push(i+1)
+
+                    unorderd.push([messages[i], messages[i].index]);
+                }   
+
+                guide.forEach(function(key) {
+                    var found = false
+                    unorderd = unorderd.filter(function(item){
+                        
+                        if(!found && item[1] == key){
+
+                            ordered.push(item[0])
+                            found = true
+                            return false
+                        }
+
+                        ordered.forEach(function(data){
+                            
+                            var name = data.name
+                            var message = data.message
+                            
+                            var message_container = document.createElement('div')
+
+
+                        })
+
+                        
+                    })
+                })
+            })
+        }
+
+
+
       }
- }
+}
